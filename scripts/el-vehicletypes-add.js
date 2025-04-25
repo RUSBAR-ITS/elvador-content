e2af804c18aa48376302b0272d72508059637a52
Ilya Barkalov <ilya.barkalov@gmail.com>
2025-04-25 22:03:10 +0000

    Base prepare ready

--- scripts/el-vehicletypes-add.js
+++ scripts/el-vehicletypes-add.js
@@ -1,7 +1,31 @@
 Hooks.once("init", () => {
   CONFIG.DND5E.toolTypes.el_vehicle = "Управление ТС";
-  CONFIG.DND5E.tools.horseriding = {
+  CONFIG.DND5E.tools.el_horseriding = {
     ability: "dex",
     id: "Compendium.elvador-content.tools.Item.X5hCPXiprevItyYS"
+  };
+  CONFIG.DND5E.tools.el_airmount = {
+    ability: "dex",
+    id: "Compendium.elvador-content.tools.Item.KiwwhuUHqs4scGOg"
+  };
+  CONFIG.DND5E.tools.el_galley = {
+    ability: "cha",
+    id: "Compendium.elvador-content.tools.Item.NoyIimAj2Wc6f2jB"
+  };
+  CONFIG.DND5E.tools.el_boat = {
+    ability: "str",
+    id: "Compendium.elvador-content.tools.Item.QmGEISA5rDDfurhp"
+  };
+  CONFIG.DND5E.tools.el_sailboat = {
+    ability: "int",
+    id: "Compendium.elvador-content.tools.Item.atJH0zK3fGc8FGpe"
+  };
+  CONFIG.DND5E.tools.el_dogriding = {
+    ability: "dex",
+    id: "Compendium.elvador-content.tools.Item.pXH6koyXKBVwIsjB"
+  };
+  CONFIG.DND5E.tools.el_coach = {
+    ability: "wis",
+    id: "Compendium.elvador-content.tools.Item.KtHgM7HkPoytRA5w"
   };
 });
